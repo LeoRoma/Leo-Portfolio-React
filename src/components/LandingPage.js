ccc932c5dc54a083d2e2afb5798630e485d1c7b7
LeoRoma <xiajtn@gmail.com>
2020-11-10 12:52:18 +0000

    completed landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,41 +1,44 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
 import {Grid, Cell} from 'react-mdl';
 import Image from 'react-bootstrap/Image';
+import {FaGithub, FaLinkedin} from 'react-icons/fa';
 
 class LandingPage extends Component {
-    render(){
-        return(
-            <div style={{width:'100%', margin:'auto'}}>
-                <Grid className="landing-grid">
-                    <Cell col={12}>
-                        {/* <Image
-                            src="./images/makersprofilepic.jpg" 
-                            rounded
-                            alt="avatar"
-                            className="avatar-img" 
-                        /> */}
-                        <div className="banner-text">
-                            <h1>Full Stack Web Developer</h1>
-                            <hr />
-                            <p>
-                                HTML/CSS | Bootstrap | Javascript | React | C# | SQL |
-                            </p>
-                            <div className="social-links">
-                                {/*Linkedin*/}
-                                <a href="https://www.linkedin.com/in/jiatian-leo-xia-9b2490191/" rel="noopenr noreferrer" target="_blank">
-                                    <i className="fa fa-linkedin-square" aria-hidden="true" />
-                                </a>    
-                                {/*Github*/}
-                                <a href="https://github.com/LeoRoma" rel="noopenr noreferrer" target="_blank">
-                                    <i className="fa fa-github-square" aria-hidden="true" />
-                                </a>   
-                            </div>
-                        </div>
-                    </Cell>
-                </Grid>
-                <h1>LandingPage Page</h1>
-            </div>
-        )
-    }
+  render(){
+      return(
+          <div style={{width:'100%', margin:'auto'}}>
+              <Grid className="landing-grid">
+                  <Cell col={12}>
+                      {/* <Image
+                          
+                          src="../images/makersprofilepic.jpg" 
+                          rounded
+                          alt="avatar"
+                          className="avatar-img" 
+                      /> */}
+                      <div className="banner-text">
+                          <h1>I am Jia Tian Xia</h1>
+                          <hr />
+                          <p>
+                            In my previous experience within Makers Academy and Sparta Global, I have learnt different coding languages such as C#, Ruby and Javascript where I have built full-stack applications using OOP and TDD, Agile, and version control. As an aspiring Junior Developer, I would like to challenge myself and my end-to-end critical thinking in a fast-paced environment.
+                          </p>
+                          <div className="social-links">
+                              {/*Linkedin*/}
+                              <a href="https://www.linkedin.com/in/jiatian-leo-xia-9b2490191/" rel="noopenr noreferrer" target="_blank">
+                                  <FaLinkedin size='3em' color='white' />
+                              </a>    
+                              
+                              {/*Github*/}
+                              <a href="https://github.com/LeoRoma" rel="noopenr noreferrer" target="_blank">
+                                <FaGithub size='3em' color='white'/>
+                              </a>   
+                          </div>
+                      </div>
+                      
+                  </Cell>
+              </Grid>
+          </div>
+      )
+  }
 }
 export default LandingPage;
