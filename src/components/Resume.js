59af8862d0f795cea0a89730d17dd105549de93e
LeoRoma <xiajtn@gmail.com>
2020-11-11 17:09:44 +0000

    complete blueprint resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,24 +7,49 @@ class Resume extends Component {
             <div className="resume">
                 <Container>
                     <Row>
-                        <Col><h1>Education</h1></Col>
-                        <Col>
-                              <Row>
-                                    <div>
-                                        <h1>Makers</h1>
-                                        <br />
-                                        <p>
-                                            Something about Makers
-                                        </p>
-                                    </div>   
-                              </Row>
-                              <Row>
-                                    <div>
-                                        <h1>University</h1>
-                                        <br />
-                                        <p>Something about Uni</p>
-                                    </div> 
-                              </Row>
+                        <Col md={3} className="education-section"><h1>Professional Experience</h1></Col>
+                        <Col md={9}>
+                            <Col sm={12}>
+                                <Row>
+                                        <div className="resume-experience">
+                                            <h1>Sparta Global</h1>
+                                            <br />
+                                            <p>
+                                                Something about Sparta
+                                            </p>
+                                        </div>   
+                                </Row>
+                                <Row>
+                                        <div className="resume-experience">
+                                            <h1>Restaurants</h1>
+                                            <br />
+                                            <p>Something about Chef</p>
+                                        </div> 
+                                </Row>
+                            </Col>                           
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={3} className="education-section"><h1>Education</h1></Col>
+                        <Col md={9}>
+                            <Col sm={12}>
+                                <Row>
+                                        <div className="resume-experience">
+                                            <h1>Makers</h1>
+                                            <br />
+                                            <p>
+                                                Something about Makers
+                                            </p>
+                                        </div>   
+                                </Row>
+                                <Row>
+                                        <div className="resume-experience">
+                                            <h1>University</h1>
+                                            <br />
+                                            <p>Something about Uni</p>
+                                        </div> 
+                                </Row>
+                            </Col>                           
                         </Col>
                     </Row>
                 </Container>
