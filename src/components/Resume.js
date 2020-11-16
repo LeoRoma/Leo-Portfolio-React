0d9cbca6d2ead90ff79f357eeeb4c6901073cabb
LeoRoma <xiajtn@gmail.com>
2020-11-16 13:31:10 +0000

    Education added

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -30,9 +30,20 @@ class Resume extends Component {
                                 </Row>
                                 <Row>
                                         <div className="resume-experience">
-                                            <h1>Restaurants</h1>
+                                            <h1>Restaurants: Sushi Hiroba, 4 Degree, Tokimeite, Kouzu</h1>
+                                            <h4>London</h4>
+                                            <h4>Sous Chef</h4>
+                                            <h4>September 2013 - December 2019</h4>
                                             <br />
-                                            <p>Something about Chef</p>
+                                            <p>
+                                                ●	Conducted  training functions, ensuring that individual performance met the highest possible culinary standards.
+                                                <br />
+                                                ●	Supervised and managed employees. Manages all day-to-day operations. Understand employee positions well enough to perform duties in employees' absence.
+                                                <br />
+                                                ●	Worked collaboratively within the team and established and maintained open communication to ensure smooth operations.
+                                                <br />
+                                                ●	Delivering  tailor-made experiences through Omakase (traditional Japanese customer-facing service).
+                                            </p>
                                         </div> 
                                 </Row>
                             </Col>                           
@@ -44,18 +55,38 @@ class Resume extends Component {
                             <Col sm={12}>
                                 <Row>
                                         <div className="resume-experience">
-                                            <h1>Makers</h1>
+                                            <h1>Makers Academy</h1>
+                                            <h4>London</h4>
+                                            <h4>Graduate Degree</h4>
+                                            <h4>May 2019 - September 2019</h4>
                                             <br />
                                             <p>
-                                                Something about Makers
+                                                ●	Test-Driven Development (TDD), Object-Oriented Design (OOD)
+                                                <br />
+                                                ●	Agile, XP, Pair Programming
+                                                <br />
+                                                ●	Full-stack development
+                                                <br />
+                                                ●	Frontend: HTML, CSS, Javascript, JQuery, Angular, React
+                                                <br />
+                                                ●	Backend: Ruby, Rails, Sinatra
+                                                <br />
+                                                ●	Testing frameworks: RSpec, Jasmine, Capybara
                                             </p>
                                         </div>   
                                 </Row>
                                 <Row>
                                         <div className="resume-experience">
-                                            <h1>University</h1>
+                                            <h1>Sapienza University Of Rome</h1>
+                                            <h4>Rome</h4>
+                                            <h4>Bachelor Degree</h4>
                                             <br />
-                                            <p>Something about Uni</p>
+                                            <p>
+                                                ●	Faculty of East Asian Languages and Cultures
+                                                <br />
+                                                ●	I have studied History, Philosophy, Religion, Literature, Philology, 
+                                                Art  History, Contemporary History, Chinese and Japanese Language.
+                                            </p>
                                         </div> 
                                 </Row>
                             </Col>                           
