f117baa2e1db030388d9992fbe16e306e7b642ee
LeoRoma <xiajtn@gmail.com>
2020-11-17 18:51:34 +0000

    fixing columns in AboutMe 2

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -9,13 +9,13 @@ class AboutMe extends Component {
                    <div className="aboutme" style={{width:'100%', margin:'auto'}}>
                     <Container className="about-grid">
                         <Row>
-                            <Col xs={4} className="aboutme-row">
+                            <Col sm={4} className="aboutme-row">
                                 {/* <div style={{height:'425px', width:'100%', margin:'auto', textAlign: 'right'}}> */}
                                     <img src={ProfilePic} className="profilepic" alt="avatar" />
                                 {/* </div> */}
                             
                             </Col>
-                            <Col xs={8} className="aboutme-row">
+                            <Col sm={8} className="aboutme-row">
                                 <Row>
                                     <h1>About Me</h1>
                                     <hr />
