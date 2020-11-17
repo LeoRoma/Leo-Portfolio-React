adf98c83b4d1186f80cf08f6e522a51be0c64605
LeoRoma <xiajtn@gmail.com>
2020-11-17 17:06:50 +0000

    Need to start contact page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,42 +1,44 @@
 import React, {Component} from 'react';
-import {Layout, Header, Navigation, Drawer} from 'react-mdl';
-// import {Link} from 'react-router-dom';
+import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+import {Link} from 'react-router-dom';
 
-// import Main from './Main';
+import Main from './Main';
 
 class Nav extends Component{
+
+    hideToggle(){
+        var selectorId = document.querySelector('.mdl-layout');
+        selectorId.MaterialLayout.toggleDrawer();
+    }
+
     render(){
         return(
             <div className="fixed-header">
                 <Layout>
-                    <Header className="header-colour" title="My Portfolio" scroll>
+                    <Header className="header-colour position-fixed" title="My Portfolio" scroll>
                         <Navigation>
-                            {/* <Link to="/">Home</Link>
+                            <Link to="/">Home</Link>
                             <Link to="/aboutme">About Me</Link>
                             <Link to="/resume">Resume</Link>
                             <Link to="/projects">Projects</Link>
-                            <Link to="/contact">Contact</Link> */}
-                            <a className="smoothscroll" href="#home">Home</a>
-                            <a className="smoothscroll" href="#about">About Me</a>
-                            <a className="smoothscroll" href="#resume">Resume</a>
-                            <a className="smoothscroll" href="#projects">Projects</a>
-                            <a className="smoothscroll" href="#contact">Contact</a>
+                            <Link to="/contact">Contact</Link>
                         </Navigation>
                     </Header>
-                    <Drawer title="My Portfolio">
-                        <Navigation>
-                            <a className="smoothscroll" href="#home">Home</a>
-                            <a className="smoothscroll" href="#about">About Me</a>
-                            <a className="smoothscroll" href="#resume">Resume</a>
-                            <a className="smoothscroll" href="#projects">Projects</a>
-                            <a className="smoothscroll" href="#contact">Contact</a>
-                        </Navigation>
-                    </Drawer>
-                    {/* <Content>
+                    <Content>
                         <div className="page-content" />
-
-                    </Content> */}
+                        <Main />
+                    </Content>
+                     <Drawer className="drawer-position" title="My Portfolio">
+                            <Navigation>
+                            <Link to="/" onClick = {() => this.hideToggle()}>Home</Link>
+                            <Link to="/aboutme" onClick = {() => this.hideToggle()}>About Me</Link>
+                            <Link to="/resume" onClick = {() => this.hideToggle()}>Resume</Link>
+                            <Link to="/projects" onClick = {() => this.hideToggle()}>Projects</Link>
+                            <Link to="/contact" onClick = {() => this.hideToggle()}>Contact</Link>
+                            </Navigation>
+                        </Drawer>
                 </Layout>
+                    
             </div>
         )
     }
