74149f26dd97b77040df9d4ba7ddf9058e15402d
LeoRoma <xiajtn@gmail.com>
2020-11-23 23:20:50 +0000

    New navbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,29 +1,38 @@
 import React, {Component} from 'react';
 import {Nav, Navbar} from 'react-bootstrap';
+import Hello from '../images/hello.jpg';
+import Ellipse from '../images/ellipse.jpg';
 
 class NavigationBar extends Component{
     render(){
         return(
             <div className="navigation-bar">
-                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                    <Navbar.Brand href="#home">My Portfolio</Navbar.Brand>
+                <Navbar className="navbar" collapseOnSelect expand="lg" bg="light" variant="light">
+                    <Navbar.Brand className="navbar-header" href="#home" style={{backgroundImage:`url("${Hello}")`, width:"187px", height:"50px"}}></Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
+                    <Navbar.Collapse className="navbar-items" id="responsive-navbar-nav">
                         <Nav className="mr-auto">
-                        {/* <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link> */}
                         </Nav>
+                        
                         <Nav>
-                        <Nav.Link href="#about">About Me</Nav.Link>
-                        <Nav.Link eventKey={2} href="#resume">
-                            Resume
-                        </Nav.Link>
-                        <Nav.Link eventKey={3} href="#projects">
-                            Projects
-                        </Nav.Link>
-                        <Nav.Link eventKey={4} href="#contact">
-                            Contact
-                        </Nav.Link>
+                            <Nav.Link className="navbar-item" href="#about">
+                                About Me /
+                            </Nav.Link>
+                            <Nav.Link eventKey={2} href="#resume">
+                                Resume /
+                            </Nav.Link>
+                            <Nav.Link eventKey={3} href="#projects">
+                                Projects /
+                            </Nav.Link>
+                            <Nav.Link eventKey={4} href="#contact">
+                                Contact /
+                            </Nav.Link>
+                            <Nav.Link eventKey={4} href="#contact">
+                                Contact /
+                            </Nav.Link>
+                            {/* <Nav.Link eventKey={5} style={{backgroundImage:`url("${Ellipse}")`, width:"3px", height:"20px"}}>
+                                
+                            </Nav.Link> */}
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
