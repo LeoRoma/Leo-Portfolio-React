7ba6a3f44d9e9c2a23374f840ae8498daf044552
LeoRoma <xiajtn@gmail.com>
2020-11-24 10:17:13 +0000

    landing page completed

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -8,14 +8,25 @@ class LandingPage extends Component {
       return(
           <section id="home">
             <div className="landingpage" style={{width:'100%', margin:'auto'}}>
-                <Container className="landingpage-container" style={{width:"100%"}}>
+                <Container className="landingpage-container" style={{width:"79%"}} fluid>
                     <Row style={{width:"100%"}}>
-                        <h1>I am Jia Tian Xia</h1>
+                        <h1>
+                            Portfolio of Jia Tian Xia, 
+                            <br />
+                            a former Sous Chef who now cooks digital products.
+                        </h1>
                     </Row>
                     <Row>
-                    <p>
-                        I am a full stack engineer driven by curiosity and a passion to learn. This passion led me to join a Bootcamp, where I built full stack applications using OOP and TDD, Agile and version control. I enjoy problem solving and I consider it a good way to stimulate and express my creativity. As an aspiring Junior Developer, I would like to challenge myself and my end-to-end critical thinking in a fast-paced environment.
-                    </p>
+                        <Col className="description-col" sm={4}>
+                            <p>
+                                I am a full stack engineer driven by curiosity and a passion to learn. This passion led me to join a Bootcamp, where I built full stack applications using OOP and TDD, Agile and version control. I enjoy problem solving and I consider it a good way to stimulate and express my creativity. 
+                            </p>
+                        </Col>
+                        <Col className="description-col" sm={4}>
+                            <p>
+                                As an aspiring Junior Developer, I would like to challenge myself and my end-to-end critical thinking in a fast-paced environment.
+                            </p>
+                        </Col>
                     </Row>
                 </Container>
             </div>
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -7,27 +7,23 @@ class NavigationBar extends Component{
     render(){
         return(
             <div className="navigation-bar">
-                <Navbar className="navbar" collapseOnSelect expand="lg" bg="light" variant="light">
+                <Navbar className="navbar" collapseOnSelect expand="lg">
                     <Navbar.Brand className="navbar-header" href="#home" style={{backgroundImage:`url("${Hello}")`, width:"187px", height:"50px"}}></Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse className="navbar-items" id="responsive-navbar-nav">
                         <Nav className="mr-auto">
                         </Nav>
-                        
                         <Nav>
-                            <Nav.Link className="navbar-item" href="#about">
+                            <Nav.Link href="#about" style={{color:'black'}}>
                                 About Me /
                             </Nav.Link>
-                            <Nav.Link eventKey={2} href="#resume">
+                            <Nav.Link eventKey={2} href="#resume" style={{color:'black'}}>
                                 Resume /
                             </Nav.Link>
-                            <Nav.Link eventKey={3} href="#projects">
+                            <Nav.Link eventKey={3} href="#projects" style={{color:'black'}}>
                                 Projects /
                             </Nav.Link>
-                            <Nav.Link eventKey={4} href="#contact">
-                                Contact /
-                            </Nav.Link>
-                            <Nav.Link eventKey={4} href="#contact">
+                            <Nav.Link eventKey={4} href="#contact" style={{color:'black'}}>
                                 Contact /
                             </Nav.Link>
                             {/* <Nav.Link eventKey={5} style={{backgroundImage:`url("${Ellipse}")`, width:"3px", height:"20px"}}>
