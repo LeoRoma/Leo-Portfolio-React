74d690dcf9cd6079e1941ed7655456070cc54cd7
LeoRoma <xiajtn@gmail.com>
2020-11-25 10:22:10 +0000

    Deleted live demo for D-witter and added rights at the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,10 @@
 import React, {Component} from 'react';
-import {FaGithub, FaLinkedin} from 'react-icons/fa';
 
 class Footer extends Component{
     render(){
         return(
             <div className="footer" style={{margin:"auto", width:"100%", background:"black", textAlign:"center"}}>
-                {/*Linkedin*/}
-                <a href="https://www.linkedin.com/in/jiatian-leo-xia-9b2490191/" rel="noopenr noreferrer" target="_blank">
-                    <FaLinkedin />
-                </a>    
-                
-                {/*Github*/}
-                <a href="https://github.com/LeoRoma" rel="noopenr noreferrer" target="_blank">
-                    <FaGithub />
-                </a>  
+               <span style={{color:"white"}}>&#64; All rights reserved</span>
             </div>
         )
     }
