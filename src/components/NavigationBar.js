182127de0af1e4da8e4aaca17c4754be1a32be18
LeoRoma <xiajtn@gmail.com>
2020-11-26 13:35:28 +0000

    Smoothscroll from css added

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,88 +1,32 @@
 import React, {Component} from 'react';
 import {Nav, Navbar} from 'react-bootstrap';
-import { Link } from "react-scroll";
-// import { MDBSmoothScroll } from "mdbreact";
 
 class NavigationBar extends Component{
+
     render(){
         return(
             <div className="navigation-bar">
                 <Navbar className="navbar" collapseOnSelect expand="lg" variant="dark">
                     <Navbar.Brand href="#home">My Portfolio</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="mr-auto">
                         </Nav>
                         <Nav>
-                        <Nav.Link>
-                            <Link
-                                href="#home"
-                                to="home"
-                                activeClass="active"
-                                className="nav-link"
-                                spy={true}
-                                smooth={true}
-                                offset={-70}
-                                duration={1000}
-                                >
-                                Home
-                            </Link>
+                        <Nav.Link href="#home">
+                            Home
                         </Nav.Link>
-                        <Nav.Link eventKey={2}>
-                            <Link
-                                href="#about"
-                                to="about"
-                                activeClass="active"
-                                className="nav-link"
-                                spy={true}
-                                smooth={true}
-                                offset={-70}
-                                duration={1000}
-                                >
-                                About Me
-                            </Link>
+                        <Nav.Link eventKey={2} href="#about">
+                            About Me
                         </Nav.Link>
-                        <Nav.Link eventKey={3}>
-                            <Link
-                                href="#resume"
-                                to="resume"
-                                activeClass="active"
-                                className="nav-link"
-                                spy={true}
-                                smooth={true}
-                                offset={-70}
-                                duration={1000}
-                                >
-                                Resume
-                            </Link>
+                        <Nav.Link eventKey={3} href="#resume">
+                            Resume
                         </Nav.Link>
-                        <Nav.Link eventKey={4}>
-                            <Link
-                                href="#projects"
-                                to="projects"
-                                activeClass="active"
-                                className="nav-link"
-                                spy={true}
-                                smooth={true}
-                                offset={-70}
-                                duration={1000}
-                                >
-                                Projects
-                            </Link>
+                        <Nav.Link eventKey={4} href="#projects">                           
+                            Projects
                         </Nav.Link>
-                        <Nav.Link eventKey={5}>
-                            <Link
-                                href="#contact"
-                                to="contact"
-                                activeClass="active"
-                                className="nav-link"
-                                spy={true}
-                                smooth={true}
-                                offset={-70}
-                                duration={1000}
-                                >
-                                Contact
-                            </Link>
+                        <Nav.Link eventKey={5} href="#contact">
+                            Contact
                         </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
