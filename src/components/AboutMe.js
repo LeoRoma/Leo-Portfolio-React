d67905fe4c6aa07c0744ffe7ce2b7bfaab303267
LeoRoma <xiajtn@gmail.com>
2020-11-30 15:28:07 +0000

    Almost completed aboutme section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -37,63 +37,35 @@ class AboutMe extends Component {
                     </div> */}
                     <div className="aboutme-container">
                         <Row className="aboutme-row">
-                            <Col xl={5} className="aboutme-text">
-                                <h1>About Me</h1>
-                                    <hr />
-                                    <p>
-                                        My primary goal is to apply my technical expertise all throughout the full software life cycle to ensure production and delivery of products and services that meet client specifications. 
-                                        <br />
-                                        <br />
-                                        Along with a competent software developing team, and with strong personal knowledge, skills, and experience in software engineering, I am positive that this goal can be achieved. 
-                                        <br />
-                                        <br />
-                                        My experience as junior software developer enhanced my abilities in designing, implementing, testing, and upgrading software. One of my objectives is to keep updated with the latest IT trends and technologies. I am confident that if given the opportunity, I can be a useful talent to the company.
-                                    </p>
-                                <div className="social-links">
-                                    <h5>FIND ME ON</h5>
-                                    <a href="https://www.linkedin.com/in/jiatian-leo-xia-9b2490191/" rel="noopenr noreferrer" target="_blank">
-                                        <FaLinkedin />
-                                    </a>    
-                                    <a href="https://github.com/LeoRoma" rel="noopenr noreferrer" target="_blank">
-                                        <FaGithub />
-                                    </a>   
-                                </div>
+                            <Col xl={6}>
+                                <div className="aboutme-text">
+                                    <h1>About Me</h1>
+                                        <hr />
+                                        <p>
+                                            My primary goal is to apply my technical expertise all throughout the full software life cycle to ensure production and delivery of products and services that meet client specifications. 
+                                            <br />
+                                            <br />
+                                            Along with a competent software developing team, and with strong personal knowledge, skills, and experience in software engineering, I am positive that this goal can be achieved. 
+                                            <br />
+                                            <br />
+                                            My experience as junior software developer enhanced my abilities in designing, implementing, testing, and upgrading software. One of my objectives is to keep updated with the latest IT trends and technologies. I am confident that if given the opportunity, I can be a useful talent to the company.
+                                        </p>
+                                    <div className="social-links">
+                                        <h5>FIND ME ON</h5>
+                                        <a href="https://www.linkedin.com/in/jiatian-leo-xia-9b2490191/" rel="noopenr noreferrer" target="_blank">
+                                            <FaLinkedin />
+                                        </a>    
+                                        <a href="https://github.com/LeoRoma" rel="noopenr noreferrer" target="_blank">
+                                            <FaGithub />
+                                        </a>   
+                                    </div>
+                                </div>                              
                             </Col>
                             <Col xl={6} className="aboutme-profilepic">
                                 <img src={ProfilePic} alt="profilepic"></img>
                             </Col>
                         </Row>
                     </div>
-                       
-                    {/* <Container className="aboutme-container" style={{width:'100%'}}>
-                        <Row>
-                            <Col className="aboutme-text">
-                                <h1>About Me</h1>
-                                    <hr />
-                                    <p>
-                                        My primary goal is to apply my technical expertise all throughout the full software life cycle to ensure production and delivery of products and services that meet client specifications. 
-                                        <br />
-                                        <br />
-                                        Along with a competent software developing team, and with strong personal knowledge, skills, and experience in software engineering, I am positive that this goal can be achieved. 
-                                        <br />
-                                        <br />
-                                        My experience as junior software developer enhanced my abilities in designing, implementing, testing, and upgrading software. One of my objectives is to keep updated with the latest IT trends and technologies. I am confident that if given the opportunity, I can be a useful talent to the company.
-                                    </p>
-                                <div className="social-links">
-                                    <h5>FIND ME ON</h5>
-                                    <a href="https://www.linkedin.com/in/jiatian-leo-xia-9b2490191/" rel="noopenr noreferrer" target="_blank">
-                                        <FaLinkedin />
-                                    </a>    
-                                    <a href="https://github.com/LeoRoma" rel="noopenr noreferrer" target="_blank">
-                                        <FaGithub />
-                                    </a>   
-                                </div>
-                            </Col>
-                            <Col className="aboutme-profilepic">
-                                <img src={ProfilePic} alt="profilepic"></img>
-                            </Col>
-                        </Row>
-                    </Container> */}
                 </div>
             </section>    
         )
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -8,97 +8,90 @@ class Resume extends Component {
                 <div className="resume">
                     <div className="resume-container">
                         <Row>
-                            <Col md={6} className="resume-experience" style={{backgroundColor:"#FFC94D"}}>
-                                <h1>PROFESSIONAL EXPERIENCE</h1>
-                                <h1>Sparta Global</h1>
-                                <h4>London</h4>
-                                <h4><i>C# Developer Trainee</i></h4>
-                                <h4>June 2020 - September 2020</h4>
-                                <br />
-                                <p>
-                                    ●	A 12-week intensive training program designed to supply key skills in C# Development and to practice throughout the academy, the mind-set, ceremonies, and continuous integration of Agile.
+                            <Col md={6}>
+                                <div className="resume-experience" style={{backgroundColor:"#FFC94D"}}>
+                                    <h1>PROFESSIONAL EXPERIENCE</h1>
+                                    <h1>Sparta Global</h1>
+                                    <h4>London</h4>
+                                    <h4><i>C# Developer Trainee</i></h4>
+                                    <h4>June 2020 - September 2020</h4>
                                     <br />
-                                    ●	Being taught C# programming language and ensuring a strong understanding of OOP programming, SOLID principles, through to building robust testable C# code with TDD and NUnit.
-                                    <br />
-                                    ●	Being able to work on team projects using ASP .NET to create full-stack applications, interacting with SQL databases using the Entity Framework and Microsoft Azure.
-                                    <br />
-                                    ●	Built a CV viewing tool for internal use using ASP .NET, AWS, Azure and Bootstrap working in an Agile environment in a team of seven. Coordinated with the Talent Management and Resource Management teams to ensure the product was meeting the business needs with weekly sprint reviews.
-                                </p>
+                                    <p>
+                                        ●	A 12-week intensive training program designed to supply key skills in C# Development and to practice throughout the academy, the mind-set, ceremonies, and continuous integration of Agile.
+                                        <br />
+                                        ●	Being taught C# programming language and ensuring a strong understanding of OOP programming, SOLID principles, through to building robust testable C# code with TDD and NUnit.
+                                        <br />
+                                        ●	Being able to work on team projects using ASP .NET to create full-stack applications, interacting with SQL databases using the Entity Framework and Microsoft Azure.
+                                        <br />
+                                        ●	Built a CV viewing tool for internal use using ASP .NET, AWS, Azure and Bootstrap working in an Agile environment in a team of seven. Coordinated with the Talent Management and Resource Management teams to ensure the product was meeting the business needs with weekly sprint reviews.
+                                    </p>
+                                </div>                               
                             </Col>
-                            <Col md={6} className="resume-experience" style={{backgroundColor:'#282C35'}}>
-                            <h1>Restaurants: Sushi Hiroba, 4 Degree, Tokimeite, Kouzu</h1>
-                                            <h4>London</h4>
-                                            <h4><i>Sous Chef</i></h4>
-                                            <h4>September 2013 - December 2019</h4>
-                                            <br />
-                                            <p>
-                                                ●	Conducted  training functions, ensuring that individual performance met the highest possible culinary standards.
-                                                <br />
-                                                ●	Supervised and managed employees. Manages all day-to-day operations. Understand employee positions well enough to perform duties in employees' absence.
-                                                <br />
-                                                ●	Worked collaboratively within the team and established and maintained open communication to ensure smooth operations.
-                                                <br />
-                                                ●	Delivering  tailor-made experiences through Omakase (traditional Japanese customer-facing service).
-                                            </p>                     
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col md={3} className="education-section"><h1>EDUCATION</h1></Col>
-                            <Col md={9}>
-                                <Col sm={12}>
-                                    <Row>
-                                        <div className="resume-experience">
-                                            <h1>Makers Academy</h1>
-                                            <h4>London</h4>
-                                            <h4><i>Graduate Degree</i></h4>
-                                            <h4>May 2019 - September 2019</h4>
-                                            <br />
-                                            <p>
-                                                ●	Test-Driven Development (TDD), Object-Oriented Design (OOD)
-                                                <br />
-                                                ●	Agile, XP, Pair Programming
-                                                <br />
-                                                ●	Full-stack development
-                                                <br />
-                                                ●	Frontend: HTML, CSS, Javascript, JQuery, Angular, React
-                                                <br />
-                                                ●	Backend: Ruby, Rails, Sinatra
-                                                <br />
-                                                ●	Testing frameworks: RSpec, Jasmine, Capybara
-                                            </p>
-                                        </div>   
-                                    </Row>
-                                    <Row>
-                                            <div className="resume-experience">
-                                                <h1>Sapienza University Of Rome</h1>
-                                                <h4><i>Rome</i></h4>
-                                                <h4>Bachelor Degree</h4>
-                                                <h4>September 2007 - December 2010</h4>
-                                                <br />
-                                                <p>
-                                                    ●	Faculty of East Asian Languages and Cultures
-                                                    <br />
-                                                    ●	I have studied History, Philosophy, Religion, Literature, Philology, 
-                                                    Art  History, Contemporary History, Chinese and Japanese Language.
-                                                </p>
-                                            </div> 
-                                    </Row>
-                                </Col>                           
+                            <Col md={6}>
+                                <div className="resume-experience" style={{backgroundColor:'#282C35'}}>
+                                    <h1>Restaurants: Sushi Hiroba, 4 Degree, Tokimeite, Kouzu</h1>
+                                    <h4>London</h4>
+                                    <h4><i>Sous Chef</i></h4>
+                                    <h4>September 2013 - December 2019</h4>
+                                    <br />
+                                    <p>
+                                        ●	Conducted  training functions, ensuring that individual performance met the highest possible culinary standards.
+                                        <br />
+                                        ●	Supervised and managed employees. Manages all day-to-day operations. Understand employee positions well enough to perform duties in employees' absence.
+                                        <br />
+                                        ●	Worked collaboratively within the team and established and maintained open communication to ensure smooth operations.
+                                        <br />
+                                        ●	Delivering  tailor-made experiences through Omakase (traditional Japanese customer-facing service).
+                                    </p>      
+                                </div>               
                             </Col>
                         </Row>
                         <Row>
-                            <Col md={3}><h1>ACHIEVEMENTS</h1></Col>
-                            <Col md={9}>
+                            <Col md={6}>
+                                <div className="resume-experience" style={{backgroundColor:"#6C58E8"}}>
+                                    <h1>EDUCATION</h1>
+                                    <h1>Makers Academy</h1>
+                                    <h4>London</h4>
+                                    <h4><i>Graduate Degree</i></h4>
+                                    <h4>May 2019 - September 2019</h4>
+                                    <br />
+                                    <p>
+                                        ●	Test-Driven Development (TDD), Object-Oriented Design (OOD)
+                                        <br />
+                                        ●	Agile, XP, Pair Programming
+                                        <br />
+                                        ●	Full-stack development
+                                        <br />
+                                        ●	Frontend: HTML, CSS, Javascript, JQuery, Angular, React
+                                        <br />
+                                        ●	Backend: Ruby, Rails, Sinatra
+                                        <br />
+                                        ●	Testing frameworks: RSpec, Jasmine, Capybara
+                                    </p>
+                                </div>                               
+                            </Col>
+
+                            <Col md={6}>
+                                <div className="resume-experience" style={{backgroundColor:'#FFFFFF'}}>
+                                    <h1>EDUCATION</h1>
+                                    <h1>Sapienza University Of Rome</h1>
+                                    <h4><i>Rome</i></h4>
+                                    <h4>Bachelor Degree</h4>
+                                    <h4>September 2007 - December 2010</h4>
+                                    <br />
+                                    <p>
+                                        ●	Faculty of East Asian Languages and Cultures
+                                        <br />
+                                        ●	I have studied History, Philosophy, Religion, Literature, Philology, 
+                                        Art  History, Contemporary History, Chinese and Japanese Language.
+                                    </p>     
+                                </div>               
+                            </Col>
+                        </Row>
+                        <Row>
                             <div className="resume-experience">
-                                    <p>
-                                        Winner of Global Sushi Challenge UK 2015 representing Kouzu Restaurant, after that I went to Tokyo, Japan to represent the UK in the Grand Final (<a href ="http://sushichallenge.global/uk-sushi-chef-xia-tia-jian-set-for-global-sushi-stardom/" rel="noopenr noreferrer" target="_blank">see link</a>). 
-                                    </p>
-                                    <br />
-                                    <p>
-                                        In 2010 I won a scholarship from Sapienza Università di Roma to study abroad at the Beijing Foreign Studies University for three months to improve Chinese language skills.
-                                    </p>
-                                </div>
-                            </Col>
+                                
+                            </div>
                         </Row>
                     </div>
                 </div>
