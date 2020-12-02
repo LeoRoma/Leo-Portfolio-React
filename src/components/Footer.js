9af2f0fed097ad6c84babbfd4fbd845360e5e80e
LeoRoma <xiajtn@gmail.com>
2020-12-02 14:35:17 +0000

    footer completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import Lens from '../images/lens.jpg';
 
 class Footer extends Component{
     render(){
         return(
-            <div className="footer" style={{margin:"auto", width:"80%", background:"black"}}>
-                <p>&#169 2020-All rights reserved</p>
+            <div className="footer">
+                <div className="footer-text">
+                    <p className="align-left"><img src={Lens} alt="lens" style={{mixBlendMode:"multiply"}}></img>Cooked with care in London, UK</p>
+                    <p className="align-right">&#169; 2020-All rights reserved</p> 
+                </div>
             </div>
         )
     }
