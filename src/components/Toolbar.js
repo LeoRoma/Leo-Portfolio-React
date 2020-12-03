7615df82ba1e167b6e0afca335b902f789bbf192
LeoRoma <xiajtn@gmail.com>
2020-12-03 16:52:01 +0000

    Change navbar color after scroll

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,48 +1,51 @@
-import React from 'react';
-import {Link} from 'react-scroll';
+import React, {useState} from 'react';
 import DrawerToggleButton from './SideDrawer/DrawerToggleButton';
 import Hello from '../images/hello.jpg';
 
-const toolbar = props => (
-    <header className="toolbar">
-        <nav className="toolbar-navigation">
-            
-            <div className="toolbar-logo">
-                <a href="#home">
-                    <img src={Hello} alt="hello" />
-                </a>
-            </div>
-            <div className="spacer" />
-            <div className="toolbar-toggle-button">
-                <DrawerToggleButton click={props.drawerToggleClickHandler} />
-            </div>
-            <div className="toolbar-navigation-items">
-                <ul>
-                    <li>
-                        <Link activeClass="active" to="about" spy={true} smooth={false} offset={50} duration={500}>
-                            About Me /
-                        </Link>
-                        {/* <a href="#about">About Me /</a> */}
-                    </li>
-                    <li>
-                        <Link activeClass="active" to="resume" spy={true} smooth={false} offset={50} duration={500}>
-                            Resume /
-                        </Link>
-                    </li>
-                    <li>
-                        <Link activeClass="active" to="projects" spy={true} smooth={false} offset={50} duration={500}>
-                            Projects /
-                        </Link>
-                    </li>
-                    <li>
-                        <Link activeClass="active" to="contact" spy={true} smooth={false} offset={50} duration={500}>
-                            Contact /
-                        </Link>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </header>
-);
+const Toolbar = props => {
+    const [toolbar, setNavbar] = useState(false);
 
-export default toolbar;
+    const changeBackground = () => {
+        if(window.scrollY >= 80){
+            setNavbar(true);
+        }else{
+            setNavbar(false);
+        }
+    };
+
+    window.addEventListener('scroll', changeBackground);
+    return(
+        <header className={toolbar ? "toolbar active" : "toolbar"}>
+            <nav className="toolbar-navigation">
+                
+                <div className="toolbar-logo">
+                    <a href="#home">
+                        <img src={Hello} alt="hello" />
+                    </a>
+                </div>
+                <div className="spacer" />
+                <div className="toolbar-toggle-button">
+                    <DrawerToggleButton click={props.drawerToggleClickHandler} />
+                </div>
+                <div className="toolbar-navigation-items">
+                    <ul>
+                        <li>
+                            <a href="#about">About Me /</a>
+                        </li>
+                        <li>
+                            <a href="#resume">Resume /</a>
+                        </li>
+                        <li>
+                            <a href="#projects">Projects /</a>
+                        </li>
+                        <li>
+                            <a href="#contact">Contact /</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    );
+};
+
+export default Toolbar;
