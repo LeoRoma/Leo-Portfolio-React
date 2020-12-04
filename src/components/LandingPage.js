29e66e50dd05548fd906ce2d762da801b503ad67
LeoRoma <xiajtn@gmail.com>
2020-12-04 10:55:00 +0000

    squash commits

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -16,12 +16,12 @@ class LandingPage extends Component {
                     </h1>
                     <div className="banner-text-col">
                         <p>
-                            I am a full stack engineer driven by curiosity and a passion to learn. This passion led me to join a Bootcamp, where I built full stack applications using OOP and TDD, Agile and version control. I enjoy problem solving and I consider it a good way to stimulate and express my creativity. 
+                            Junior full-stack engineer driven by curiosity and a passion to learn. This passion led me to join a Bootcamp, where I built full-stack applications using OOP, TDD, Agile, and version control. I enjoy problem-solving and I consider it a good way to stimulate and express my creativity. I have developed strong interpersonal skills through a wide range of work experience. 
                         </p>
                     </div>
                     <div className="banner-text-col">
                         <p>
-                            As an aspiring Junior Developer, I would like to challenge myself and my end-to-end critical thinking in a fast-paced environment.
+                            As an aspiring Junior Software Developer, I would like to challenge myself and my end-to-end critical thinking in a fast-paced environment.
                         </p>
                     </div>
                 </div>  
